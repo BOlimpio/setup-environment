845a4124bb252e7786dfacfa7178e6c3534f1c6f
BRUNO OLIMPIO <b.olimpio@outlook.com>
2022-06-16 17:10:42 -0300

    add v0 of script to install basic tools

---
+++ ubuntu-environment-setup.sh
@@ -0,0 +1,81 @@
+#install vs code repositories from microsoft 
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
+echo "\n"
+code --version
+echo "\n"
+
+#install notepad++ with snap command
+sudo snap install notepad-plus-plus
+
+#install docker
+sudo apt-get update
+sudo apt-get install \
+   ca-certificates \
+   curl \
+   gnupg \
+   lsb-release  
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo "\n"
+docker-version
+echo "\n"
+
+#install kubectl
+sudo apt-get update
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl  
+echo "\n"
+kubectl version
+echo "\n"
+  
+#install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+echo "\n"
+eksctl version
+echo "\n"
+  
+# install awscli2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+echo "\n"
+aws --version
+echo "\n"
+
+#install python3
+sudo apt-get install -y python3
+echo "\n"
+python -V
+echo "\n"
+
+#install pip package manager
+sudo apt-get install -y python3-pip
+echo "\n"
+pip -V
+echo "\n"
+
+#install git
+sudo apt-get install -y git-all
+echo "\n"
+git --version
+echo "\n"  
+  
+  
+  
+  
+  
