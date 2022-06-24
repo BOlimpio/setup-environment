ffee5f8a141cdeaf5756b4e4125613a11b799c04
BRUNO OLIMPIO <b.olimpio@outlook.com>
2022-06-24 19:48:35 -0300

    add terraform installer

--- ubuntu-environment-setup.sh
+++ ubuntu-environment-setup.sh
@@ -75,7 +75,13 @@ echo "\n"
 git --version
 echo "\n"  
   
-  
-  
-  
-  
+#install terraform
+sudo apt-get update
+sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update
+sudo apt-get install -y terraform
+echo "\n"
+terraform -version
+echo "\n"
