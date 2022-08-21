4115b38123cb3f28903240862b5865e666c9f54a
BOlimpio <b.olimpio@outlook.com>
2022-08-21 13:08:25 -0300

    add node.js and angular cli installation

--- ubuntu-environment-setup.sh
+++ ubuntu-environment-setup.sh
@@ -105,3 +105,12 @@ sudo systemctl status jenkins
 
 #install python virtual environment
 python -m pip install --user virtualenv
+
+#install node.js
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install nodejs
+node -v 
+
+#install angular cli
+sudo npm install -g @angular/cli
